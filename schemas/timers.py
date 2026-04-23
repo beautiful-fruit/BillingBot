775f607b20fd33c1dfc21d6d188aee477b504c05
CHIH-HAO, CHUANG <contact@chih-hao.xyz>
2026-04-24 03:49:34 +0800

    Update timer respository

--- schemas/timers.py
+++ schemas/timers.py
@@ -18,5 +18,5 @@ class TimerData(BaseModel):
     def insert_query(self) -> tuple[str, tuple]:
         return """
             INSERT INTO timers (id, channel_id, user_id, trigger_time, message, original_message)
-            VALUES ($1, $2, $3, $4, $5)
+            VALUES ($1, $2, $3, $4, $5, $6)
         """, (self.id.value, self.channel_id, self.user_id, self.trigger_time, self.message, self.original_message)
